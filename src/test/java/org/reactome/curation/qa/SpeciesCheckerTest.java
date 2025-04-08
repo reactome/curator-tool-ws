2ba4fe4650c9540990b4904abdd73669eda2969c
guanmingwu <guanmingwu@gmail.com>
2025-04-08 12:10:11 -0700

    merged the pull request from TestCases

--- src/test/java/org/reactome/curation/qa/SpeciesCheckerTest.java
+++ src/test/java/org/reactome/curation/qa/SpeciesCheckerTest.java
@@ -8,10 +8,18 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.reactome.curation.service.CurationService;
+import org.reactome.server.graph.domain.model.BlackBoxEvent;
 import org.reactome.server.graph.domain.model.CatalystActivity;
+import org.reactome.server.graph.domain.model.CellDevelopmentStep;
 import org.reactome.server.graph.domain.model.Complex;
+import org.reactome.server.graph.domain.model.DefinedSet;
+import org.reactome.server.graph.domain.model.Depolymerisation;
+import org.reactome.server.graph.domain.model.EntitySet;
 import org.reactome.server.graph.domain.model.EntityWithAccessionedSequence;
+import org.reactome.server.graph.domain.model.Event;
+import org.reactome.server.graph.domain.model.FailedReaction;
 import org.reactome.server.graph.domain.model.LiteratureReference;
+import org.reactome.server.graph.domain.model.Pathway;
 import org.reactome.server.graph.domain.model.PhysicalEntity;
 import org.reactome.server.graph.domain.model.Publication;
 import org.reactome.server.graph.domain.model.Reaction;
@@ -32,6 +40,88 @@ class SpeciesCheckerTest {
     // Note: This is needed. 
     @Test
     void contextLoads() {
+    }
+    
+    private EntitySet createEntitySet() {
+        Long dbId = -1L;
+
+        DefinedSet entitySet = new DefinedSet();
+        entitySet.setDbId(dbId--);
+        entitySet.setDisplayName("EntitySet for Compartment Test");
+        Long[] speciesIds = { 48887L, 164493L };
+        List<Species> species = createSpeciesList(speciesIds);
+        entitySet.setSpecies(species);
+
+        EntityWithAccessionedSequence member1 = new EntityWithAccessionedSequence();
+        member1.setDbId(dbId--);
+        member1.setDisplayName("First Member");
+        member1.setSpecies(createSpecies(451465L));
+
+        EntityWithAccessionedSequence member2 = new EntityWithAccessionedSequence();
+        member2.setDbId(dbId--);
+        member2.setDisplayName("Second Member");
+        member2.setSpecies(createSpecies(451465L));
+
+        List<PhysicalEntity> members = new ArrayList<>();
+        members.add(member1);
+        members.add(member2);
+        entitySet.setHasMember(members);
+
+        return entitySet;
+    }
+
+    private Pathway createPathway() {
+        Long dbId = -1L;
+        Pathway pathway = new Pathway(dbId--);
+        pathway.setDisplayName("Pathway for Species Check");
+        Long[] speciesIds = { 48887L, 164493L };
+        List<Species> species = createSpeciesList(speciesIds);
+        pathway.setSpecies(species);
+
+        Event event1 = new BlackBoxEvent();
+        event1.setDbId(dbId--);
+        event1.setDisplayName("Test BlackBox Event");
+        speciesIds = new Long[] { 451465L, 159879L };
+        species = createSpeciesList(speciesIds);
+        event1.setSpecies(species);
+
+        Reaction event2 = new Reaction();
+        event2.setDbId(dbId--);
+        event2.setDisplayName("Test Reaction");
+        speciesIds = new Long[] { 2671792L, 48898L };
+        species = createSpeciesList(speciesIds);
+        event2.setSpecies(species);
+
+        Event event3 = new CellDevelopmentStep();
+        event3.setDbId(dbId--);
+        event3.setDisplayName("Test Cell Development Event");
+        speciesIds = new Long[] { 451465L, 62596L };
+        species = createSpeciesList(speciesIds);
+        event3.setSpecies(species);
+
+        Event event4 = new Depolymerisation();
+        event4.setDbId(dbId--);
+        event4.setDisplayName("Test Depolymerisation Event");
+        speciesIds = new Long[] { 451465L, 9603609L };
+        species = createSpeciesList(speciesIds);
+        event4.setSpecies(species);
+
+        Event event5 = new FailedReaction();
+        event5.setDbId(dbId--);
+        event5.setDisplayName("Failed Reaction");
+        speciesIds = new Long[] { 451465L, 68323L };
+        species = createSpeciesList(speciesIds);
+        event5.setSpecies(species);
+
+        List<Event> events = new ArrayList<>();
+        events.add(event1);
+        events.add(event2);
+        events.add(event3);
+        events.add(event4);
+        events.add(event5);
+
+        pathway.setHasEvent(events);
+        return pathway;
     }
 
     private ReactionLikeEvent createReactionLikeEvent() {
@@ -83,6 +173,30 @@ class SpeciesCheckerTest {
 //        Long dbId = rtn.getDbId();
 //        System.out.println("New reaction created: " + rtn);
         Long dbId = 9851588L;
+        checkSpecies(dbId);
+    }
+    
+    @Test
+    public void testPathwaySpeciesCheck() throws Exception {
+//        // Create a new pathway
+//        Pathway pathway = createPathway();
+//        DatabaseObject rtn = curationService.commit(pathway);
+////         Check this reaction
+//        Long dbId = rtn.getDbId();
+//        System.out.println("New pathway created: " + rtn);
+        Long dbId = 9851338L;
+        checkSpecies(dbId);
+    }
+
+    @Test
+    public void testEntitySetSpeciesCheck() throws Exception {
+//        // Create a new entitySet
+//        EntitySet entitySet = createEntitySet();
+//        DatabaseObject rtn = curationService.commit(entitySet);
+////         Check this reaction
+//        Long dbId = rtn.getDbId();
+//        System.out.println("New entitySet created: " + rtn);
+        Long dbId = 9851327L;
         checkSpecies(dbId);
     }
 
