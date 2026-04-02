b43e062585c8294875febb79f5cbf9bfde00e2bf
guanmingwu <guanmingwu@gmail.com>
2026-04-02 14:44:28 -0700

    Add @JsonIgnore annotation to isEmpty method in InstanceList and update pom.xml for repackage execution

--- src/main/java/org/reactome/curation/model/InstanceList.java
+++ src/main/java/org/reactome/curation/model/InstanceList.java
@@ -2,6 +2,7 @@ package org.reactome.curation.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -16,7 +17,9 @@ public class InstanceList {
     // Total count of the query results.
     // This number may be different from the size of the instances list, which is determined by the page size.
     private Integer totalCount;
-    
+
+    // Ignore this method when serializing to JSON, as it is a utility method for checking if the list is empty.
+    @JsonIgnore
     public boolean isEmpty() {
         return instances == null || instances.isEmpty();
     }
